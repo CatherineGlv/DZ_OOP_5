e04b7a8e3a0441f34328ea97071ac4a387c80c9e
CatherineGlv <cool_kiti@mail.ru>
2021-04-01 21:48:59 +1000

    Update

---
+++ DZ_OOP_5.1.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+
+template <class T>
+class Pair
+{
+private:
+	T m_a;
+	T m_b;
+
+public:
+	Pair(const T& a, const T& b) : m_a(a), m_b(b)
+	{
+	}
+
+	T& first() { 
+		return m_a; 
+	}
+	
+	const T& first() const { 
+		return m_a; 
+	}
+	
+	T& second() { 
+		return m_b; 
+	}
+	
+	const T& second() const { 
+		return m_b; 
+	}
+};
+
+int main()
+{
+	Pair<int> p1(6, 9);
+	std::cout << "Pair: " << p1.first() << ' ' << p1.second() << '\n';
+
+	const Pair<double> p2(3.4, 7.8);
+	std::cout << "Pair: " << p2.first() << ' ' << p2.second() << '\n';
+
+	return 0;
+}
